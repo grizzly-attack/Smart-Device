0b0740aefd7940fae874ad2d53a3dfd69df9ae55
Eugenia <janevega25@gmail.com>
2019-09-10 12:57:58 +0300

    Доработка по критериям качества

---
+++ source/js/main.js
@@ -0,0 +1,55 @@
+  'use strict';
+
+  var toggle = document.querySelector('.page-footer__toggle');
+  var footerBlock = document.querySelector('.page-footer__block');
+
+  var openButton = document.querySelector('.page-header__button');
+  var popup = document.querySelector('.popup');
+  var close = popup.querySelector('.popup__close-button');
+
+  var form = popup.querySelector('.popup__form');
+  var userName = popup.querySelector('[name="user-name"]');
+  var phone = popup.querySelector('[name="user-phone"]');
+  var question = popup.querySelector('[name="user-question"]');
+
+  toggle.classList.remove('page-footer__toggle--js-locked');
+  footerBlock.classList.remove('page-footer__block--js-locked');
+  toggle.addEventListener('click', function() {
+    if (toggle.classList.contains('page-footer__toggle--closed')) {
+      toggle.classList.remove('page-footer__toggle--closed');
+      toggle.classList.add('page-footer__toggle--opened');
+      footerBlock.classList.remove('page-footer__block--hidden');
+    }
+    else {
+      toggle.classList.add('page-footer__toggle--closed');
+      toggle.classList.remove('page-footer__toggle--opened');
+      footerBlock.classList.add('page-footer__block--hidden');
+    }
+  });
+
+  openButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.add('popup--show');
+    userName.focus();
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("popup--show");
+  });
+
+  form.addEventListener("submit", function () {
+    localStorage.setItem("name", name.value);
+    localStorage.setItem("phone", phone.value);
+    localStorage.setItem("question", question.value);
+  });
+
+  window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("popup--show")) {
+      popup.classList.remove("popup--show");
+      }
+    }
+  });
+
